cfd8ef1c16ef94d2a58c58e76de36fb67e5ebba9
Fernando Stadler <fernandostadler2@gmail.com>
2023-01-12 11:43:36 -0300

    Refatoração do código

---
+++ app.py
@@ -0,0 +1,46 @@
+import random;
+
+choices = ["pedra","papel","tesoura"]
+
+def computerChoice():
+   computerChoiceReturn = random.choice(choices)
+   return computerChoiceReturn
+
+def selectChoice(str):
+     playerChoice = choices[str]
+     return playerChoice;
+     
+def play():
+     selected = input("Faça sua escolha [1 para pedra] - [2 para papel] - [3 para tesoura]: ")
+     toInt = int(selected)
+     normalize = toInt-1
+
+     move = selectChoice(normalize);
+     cpu = computerChoice();
+     print("Escolha do jogador: " + move+" "+" Escolha do computador: "+cpu)
+
+     if move == "pedra" and cpu == "tesoura":
+          print("você ganhou!")
+     elif move == "papel" and cpu == "pedra":
+          print("você ganhou!")
+     elif move == "teroura" and cpu =="papel":
+          print("você ganhou!")
+     elif move == cpu:
+          print("O jogo empatou!")    
+     else:
+          print("Voce perdeu!")
+play();
+
+
+
+
+
+
+
+            
+
+    
+
+
+
+
